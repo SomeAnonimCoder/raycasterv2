4e0290d01bfe2d84409bad922d414f53f3573e03
Some Anonim Coder <aiglubshev@edu.hse.ru>
2020-02-06 16:22:54 +0300

    Fix cython rendering.

--- render.pyx
+++ render.pyx
@@ -3,6 +3,8 @@ from math import cos, tan, sqrt, sin, pi
 import PIL.Image, PIL.ImageDraw, PIL.ImageColor
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 from field import Field
 from player import Player
 
@@ -24,16 +26,25 @@ cpdef render_map(map, player):
         fill="RED")
     return frame
 
+cdef struct Column:
+    int color
+    float height
+ctypedef Column column
+
+cdef column heights[640]
+
+
 # get array of heights for each pixel column
-cpdef get_heights(field, player):
-    heights = []
+cdef column* get_heights(field, player):
     # for ray in field of view
     cdef float alpha
     cdef float k
     cdef float y_0
     cdef float x, y, dist, tmp
-    for i in range(0, WIDTH):
-        alpha = player.v_dir - player.v_field / 2 + player.v_field * i / WIDTH
+    cdef column col
+
+    for ray_num in range(0, WIDTH):
+        alpha = player.v_dir - player.v_field / 2 + player.v_field * ray_num / WIDTH
         k = tan(alpha)
         y_0 = player.y - k * player.x
 
@@ -54,11 +65,14 @@ cpdef get_heights(field, player):
             if tmp < dist:
                 dist = tmp
                 color = collision[2]
-        heights.append((1 / dist / cos(-alpha + player.v_dir) if 0 != dist else 1000, color))
+        col.height = 1 / dist / cos(-alpha + player.v_dir) if 0 != dist and cos(player.v_dir-alpha)!=0 else 1000
+        col.color = color
+        heights[ray_num] = col
+        #print("creating",ray_num, col.height, col.color)
     return heights
 
 # count coordinates of collision with block(i,j) for ray y = y_0 + kx
-def block_cross(i, j, k, y_0, alpha, player):
+cdef block_cross(int i, int j, float k, float y_0, float alpha, player):
     # cell coordinates
     cdef float x_cell = i * H
     cdef float y_cell = j * H
@@ -103,15 +117,34 @@ def block_cross(i, j, k, y_0, alpha, player):
 
     return x, y, dist
 
-cpdef create_image(heights):
-    frame = []
-    for height, color in heights:
-        col = [255 for _ in range(0, max(int(HEIGHT / 2 - int(height * 100)), 0))] + \
-              [color for _ in range(0, min(int(height * 100), int(HEIGHT / 2)))] + \
-              [color for _ in range(0, min(int(height * 40), int(HEIGHT / 2)))] + \
-              [127 for _ in range(0, max(int(HEIGHT / 2 - int(height * 40)), 0))]
-        frame.append(col)
-    frame = np.array(frame).transpose()
+cpdef create_image(field, player):
+    heights = get_heights(field, player)
+    cdef int frame[480][640]
+    cdef float height
+    cdef int color
+    cdef column col
+    for i in range(0, WIDTH):
+
+        height = heights[i].height
+        color = heights[i].color
+
+        #print(i, height, color)
+
+        wall_top = int(HEIGHT/2+height*100)
+        if wall_top > HEIGHT: wall_top=HEIGHT
+        wall_bottom = int(HEIGHT/2 - height*40)
+        if wall_bottom <0: wall_bottom=0
+
+        for j in range(0, wall_bottom):
+            frame[j][i] = 255
+
+        for j in range(wall_bottom, wall_top):
+            frame[j][i] = 0
+
+        for j in range(wall_top, HEIGHT):
+            frame[j][i] = 127
+
+
     return PIL.Image.fromarray(np.uint8(frame))
 
 #
